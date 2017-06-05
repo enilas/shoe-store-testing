664fcd777a1dc782eea7d402279e9bdd43b0cb2d
Paul Nguyen <paulng91@gmail.com>
2017-06-05 19:58:06 -0400

    Gutted email input verification, modularized month tests
    I realized I was checking the wrong field for reminder e-mails. Since this is kept in the Git history, it can be referred to if  needed to check every shoe result for an e-mail input text box. It's not necessary according to the requirements at the moment, so I gutted the method to replace it with the reminder e-mails check.

--- test/shoe_store_testing/core_test.clj
+++ test/shoe_store_testing/core_test.clj
@@ -40,19 +40,7 @@
 (defn submit-email-inputs
   "Inputs notification_email_input text boxes."
   [month]
-  (let [email-amounts (count (browser/find-elements-by-class-name "notification_email_input"))]
-    ;; Testing to check if email input fields are available in each result.
-    (is (= email-amounts (count (browser/find-elements-by-class-name "shoe_result"))) "Email input field missing from at least one result.")
-    ;; Testing to check if each email input field works.
-    (for [index (range email-amounts)]
-      (do (let [input (nth (browser/find-elements-by-class-name "notification_email_input") index)]
-            (element/send-keys input "paulng91@gmail.com")
-            (element/submit input)
-            (wait/instance 10)
-            (let [notice (browser/find-element-by-class-name "notice")]
-              (is (= (element/get-text notice) "Thanks! We will notify you when this shoe is available at this email: paulng91@gmail.com"))
-              (Thread/sleep 1000)
-              (browser/get (str "http://shoestore-manheim.rhcloud.com/months/" month))))))))
+  (let [reminder-email-inputs (browser/find-elements-by-class-name "remind_email_input")]))
 
 (defn test-display
   "Tests the display of each shoe result."
@@ -89,137 +77,112 @@
     (is (not (nil? (browser/instance))))
     (firefox/quit)))
 
-(defn january-display-test
-  []
-  (testing "Should return Description, Picture, and Pricing for January."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/january")
-    (wait/instance 10)
-    (test-display)))
-
 (deftest january-browser-test
   (testing "Testing browser compatibility for January."
     (let [_ (chrome/start-chrome)]
-      (january-display-test)
-      (submit-email-inputs "january"))))
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/january")
+      (wait/instance 10)
+      (test-display)
+      (submit-email-inputs "january")
+      (chrome/quit))))
 
 
-(defn february-display-test
+(deftest february-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for February."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/february")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/february")
+      (wait/instance 10)
+      (test-display)
+      (submit-email-inputs "february")
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn march-display-test
+(deftest march-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for March."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/march")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/march")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn april-display-test
+(deftest april-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for April."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/april")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/april")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn may-display-test
+(deftest may-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for May."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/may")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/may")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn june-display-test
+(deftest june-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for June."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/june")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/june")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn july-display-test
+(deftest july-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for July."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/july")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/july")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn august-display-test
+(deftest august-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for August."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/august")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/august")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn september-display-test
+(deftest september-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for September."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/september")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/september")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn october-display-test
+(deftest october-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for October."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/october")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/october")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn november-display-test
+(deftest november-browser-test
   []
   (testing "Should return Description, Picture, and Pricing for November."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/november")
-    (wait/instance 10)
-    (test-display)))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/november")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
 
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
-
-(defn december-display-test
+(deftest december-display-test
   []
   (testing "Should return Description, Picture, and Pricing for December."
-    (browser/get "http://shoestore-manheim.rhcloud.com/months/december")
-    (wait/instance 10)
-    (test-display)))
-
-(defn january-email-test
-  []
-  (testing "Should receive successful message when e-mail is submitted."))
+    (let [_ (chrome/start-chrome)]
+      (browser/get "http://shoestore-manheim.rhcloud.com/months/december")
+      (wait/instance 10)
+      (test-display)
+      (chrome/quit))))
